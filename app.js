f9a90e7501327b48d76d014be8362ede080042bd
Thomas <jtruant@gmx.com>
2016-01-14 16:21:00 -0700

    don't add single IP multiple times.

--- app.js
+++ app.js
@@ -13,23 +13,34 @@ var log = grid.set(0, 1, 1, 1, contrib.log, {
     , label: "Server Log"
     });
 
+var processed = [];
+
 function markVisitors(file) {
+
   fs.readFile(file, 'utf8', function (err, data) {
       if (err) throw err;
+
       var ips = data.split("\n")
-      for (var i=0; i < ips.length; i++) {
-          var gdat = geoip.lookup(ips[i]);
-          if (gdat == null) continue;
-          log.log(gdat.country);
-          map.addMarker({"lon": gdat.ll[1], "lat": gdat.ll[0], color: "red", char: "X"})
-      }
+      ips.forEach(function (ip, i, arr) {
+        var gdat = geoip.lookup(ip);
+
+        if (gdat == null) return;
+        if (processed.indexOf(ip) != -1) return;
+
+        log.log(gdat.country);
+        map.addMarker({"lon": gdat.ll[1], "lat": gdat.ll[0], color: "red", char: "X"});
+        processed.push(ip);
+      });
   });
   screen.render()
 }
 
-setInterval(function() { markVisitors('iplist') }, 1000)
+setInterval(function() {
+  markVisitors('iplist')
+}, 1000);
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
     return process.exit(0);
 });
+
 screen.render()
