7abd7173d021883f0848e13d879af971912756ac
Thomas <jtruant@gmx.com>
2016-01-14 02:45:07 -0700

    added log widget

--- app.js
+++ app.js
@@ -6,25 +6,30 @@ var blessed = require('blessed')
 var geoip = require('geoip-lite')
 var fs = require('fs')
 
-var map = grid.set(0, 0, 1, 2, contrib.map, {label: 'Visitors Location'})
+var map = grid.set(0, 0, 1, 1, contrib.map, {label: 'Visitors Location'})
+var log = grid.set(0, 1, 1, 1, contrib.log, {
+    fg: "green"
+    , selectedFg: "green"
+    , label: "Server Log"
+    });
 
-fs.readFile('iplist', 'utf8', function (err, data) {
-    if (err) throw err;
-    var ips = data.split("\n")
-    for (var i=0; i < ips.length; i++) {
-        var gdat = geoip.lookup(ips[i]);
-        if (gdat == null) continue;
-        console.dir(gdat.ll)
-        map.addMarker({"lon": gdat.ll[1], "lat": gdat.ll[0], color: "red", char: "X"})
-    }
-});
+function markVisitors(file) {
+  fs.readFile(file, 'utf8', function (err, data) {
+      if (err) throw err;
+      var ips = data.split("\n")
+      for (var i=0; i < ips.length; i++) {
+          var gdat = geoip.lookup(ips[i]);
+          if (gdat == null) continue;
+          log.log(gdat.country);
+          map.addMarker({"lon": gdat.ll[1], "lat": gdat.ll[0], color: "red", char: "X"})
+      }
+  });
+  screen.render()
+}
+
+setInterval(function() { markVisitors('iplist') }, 1000)
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
     return process.exit(0);
 });
-
-function run() {
-    screen.render()
-}
-
-setInterval(run, 1000)
+screen.render()
